553341f8fe82a6bb2a4f03f698cd28be9beb41c3
AXIERGONZALEZPEREZ <asiergperez@gmail.com>
2026-02-23 19:58:11 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-import express from "express";
-import cors from "cors";
-import crypto from "crypto";
-
+const express = require("express");
+const cors = require("cors");
+const crypto = require("crypto");
 const app = express();
 app.use(cors());
 app.use(express.json());
